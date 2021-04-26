66df27633fb5fd992df3701b32b7c58c7d60f4ca
Hijodeagua <73750349+Hijodeagua@users.noreply.github.com>
2021-04-26 11:59:17 -0500

    Update app.js

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     var samples= data.samples;
     var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
-    var result= resultsarray[0]
+    var result= resultsarray[0];
 
     var otu_ids = result.otu_ids;
     var otu_labels = result.otu_labels;
